83bb89454554da9b1576375b863b0c2782638cc1
Marco Bassaletti <bassaletti@gmail.com>
2022-02-24 23:25:58 -0800

    [Glox][ASTPrinter] Use methods to build expressions instead of direct struct construction

--- glox/astprinter_test.go
+++ glox/astprinter_test.go
@@ -7,20 +7,11 @@ import (
 )
 
 func TestAstPrinterPrint(t *testing.T) {
-	expression := BinaryExpr{
-		Left: UnaryExpr{
-			Operator: NewToken(TOKEN_MINUS, "-", nil, 1),
-			Right: LiteralExpr{
-				Value: 123,
-			},
-		},
-		Operator: NewToken(TOKEN_STAR, "*", nil, 1),
-		Right: GroupingExpr{
-			Expression: LiteralExpr{
-				Value: 45.67,
-			},
-		},
-	}
+	expression := NewBinaryExpr(
+		NewUnaryExpr(NewToken(TOKEN_MINUS, "-", nil, 1), NewLiteralExpr(1, 123)),
+		NewToken(TOKEN_STAR, "*", nil, 1),
+		NewGroupingExpr(NewLiteralExpr(1, 45.67)),
+	)
 	astPrinter := AstPrinter{}
 	assert.Equal(t, "(* (- 123) (group 45.67))", astPrinter.Print(expression))
 }
